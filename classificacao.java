cad79e0ec4f3102a6d4fde55c1ae4af47dd786ad
Carol Koga <caroltkoga@gmail.com>
2025-04-17 09:21:56 -0300

    Classificação de idade em campeonato

--- classificacao.java
+++ classificacao.java
@@ -14,15 +14,43 @@ public class classificacao{
         for (i=1 ; i<=3 ; i ++){
             //nome do usuario com verificação de caracter:
             while(true){ //vai rodar até que o usuário digite pelo menos um caractere
-            System.out.println("Candidato número #" + i);
-            System.out.println("Digite o nome: ");
-            nome = scanner.nextLine().trim();
-            if (!nome.isEmpty()){
-                break;
-            } else {
-                System.out.println("O nome não pode estar vazio.");Teste
-            }
+                System.out.println();
+                System.out.println("Candidato número #" + i);
+                System.out.println("Digite o nome: ");
+                nome = scanner.nextLine().trim();
+                if (!nome.isEmpty()){ //verifica se o campo esta vazio.
+                    break;
+                } else { //caso o campo estiver vazio
+                    System.out.println();
+                    System.out.println("____________________________");
+                    System.out.println("O nome não pode estar vazio.");
+                    System.out.println("____________________________");
+                    System.out.println();
+                }
             } 
+            //cadastro do ano de nascimento de cada usuário:
+            while(true){
+                System.out.println("Digite o ano de nascimento: ");
+                nascimento = scanner.nextInt();
+                scanner.nextLine();
+                if (nascimento >= 1950 && nascimento <= atual){
+                    break;
+                } else {
+                    System.out.println("____________________________");
+                    System.out.println("É preciso estar entre 1950 e " + atual);
+                    System.out.println("____________________________");
+                    System.out.println();
+                }
+            }
+                idade = (atual - nascimento);
+                System.out.println("A idade de " + nome + " é: " + idade);
+                if (idade > 25){
+                    System.out.println(nome + " esta classificado para a liga Premium.");
+                } else if (idade < 25 && idade >= 18) {
+                    System.out.println(nome + " esta classificado para a liga Junior.");
+                }else {
+                    System.out.println(nome + " não pode participar do campeonato.");
+                }
             }
         }
     }
