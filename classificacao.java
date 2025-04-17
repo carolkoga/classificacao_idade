68b9aa5ae686e15157999162b10a2ec43ba45ae1
Carol Koga <caroltkoga@gmail.com>
2025-04-17 08:05:16 -0300

    campo cadastro nome

--- classificacao.java
+++ classificacao.java
@@ -1,9 +1,9 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class classificacao{ 
     public static void main (String[] args){ 
         //declaração de variáveis
         Scanner scanner = new Scanner(System.in);
-        string nome;
+        String nome;
         int nascimento;
         int atual;
         int idade;
@@ -13,7 +13,16 @@ public class classificacao{
         //cadastrar 3 campos:
         for (i=1 ; i<=3 ; i ++){
             //nome do usuario com verificação de caracter:
+            while(true){ //vai rodar até que o usuário digite pelo menos um caractere
+            System.out.println("Candidato número #" + i);
+            System.out.println("Digite o nome: ");
+            nome = scanner.nextLine().trim();
+            if (!nome.isEmpty()){
+                break;
+            } else {
+                System.out.println("O nome não pode estar vazio.");Teste
+            }
+            } 
             }
         }
     }
-};
