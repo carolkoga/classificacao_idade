93851c0f54764c5ef117c9217a2f1f375680f4dd
Carol Koga <caroltkoga@gmail.com>
2025-04-16 22:09:58 -0300

    primeira parte sistema

---
+++ classificacao.java
@@ -0,0 +1,18 @@
+import java.util.Scanner
+public class classificacao{ 
+    public static void main (String[] args){ 
+        //declaração de variáveis
+        Scanner scanner = new Scanner(System.in);
+        string nome;
+        int nascimento;
+        int atual;
+        int idade;
+        int i;
+        atual = 2025;
+
+        //cadastrar 3 campos:
+        for (i=1 ; i<=3 ; i ++){
+
+        }
+    }
+};
